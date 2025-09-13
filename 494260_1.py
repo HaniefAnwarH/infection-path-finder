3c1259b7109450711252fd373416bd031d40c67f
Hanief Anwar <haniefhahay@gmail.com>
2025-09-13 14:40:26 +0700

    add infection path finder .py code

---
+++ 494260_1.py
@@ -0,0 +1,52 @@
+import sys
+from collections import deque
+
+def solve_infection_path():
+    try:
+        n_str, m_str = sys.stdin.readline().split()
+        N = int(n_str)
+        M = int(m_str)
+    except (IOError, ValueError):
+        return
+
+    adj = {i: [] for i in range(1, N + 1)}
+    for _ in range(M):
+        u_str, v_str = sys.stdin.readline().split()
+        u, v = int(u_str), int(v_str)
+        adj[u].append(v)
+        adj[v].append(u)
+
+    s_str, t_str = sys.stdin.readline().split()
+    S = int(s_str)
+    T = int(t_str)
+
+    queue = deque([S])
+    visited = {S}
+    parent = {S: None}
+
+    path_found = False
+    while queue:
+        current_node = queue.popleft()
+        if current_node == T:
+            path_found = True
+            break
+        
+        for neighbor in adj[current_node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                parent[neighbor] = current_node
+                queue.append(neighbor)
+    
+    if path_found:
+        path = []
+        curr = T
+        while curr is not None:
+            path.append(str(curr))
+            curr = parent.get(curr)
+        
+        print(" ".join(path[::-1]))
+    else:
+        print(-1)
+
+if __name__ == "__main__":
+    solve_infection_path()
